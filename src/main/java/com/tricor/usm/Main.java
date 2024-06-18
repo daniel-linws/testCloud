1632722db4a9f65217cc2a61043a24d00cbfc4fd
danielwslin <daniel.ws.lin@hk.tricorglobal.com>
2024-06-18 18:43:59 +0800

    added

--- src/main/java/com/tricor/usm/Main.java
+++ src/main/java/com/tricor/usm/Main.java
@@ -2,6 +2,8 @@ package com.tricor.usm;
 
 public class Main {
     public static void main(String[] args) {
+        int a =0;
+        int b = 9/a;
         System.out.println("Hello world!");
     }
 }
